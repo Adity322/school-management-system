bcf35d0abe4535cf277ccd4fb12dcf0878ddfa67
Aditya kumar singh <adityakumarsingh@Adityas-MacBook-Air.local>
2026-04-10 21:51:57 +0530

    Removed local host

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import BASE_URL from "../api";
 const Login = () => {
   const [form, setForm] = useState({
     email: "",
